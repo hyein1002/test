7a912b1543991c7309f5f7c66e0b246247bd1bcd
hyein1002 <hyene02@gmail.com>
2020-03-23 16:18:46 +0900

    hotfixtest22

--- dev_book/src/hotfix/HotFixTest.java
+++ dev_book/src/hotfix/HotFixTest.java
@@ -3,8 +3,8 @@ package hotfix;
 public class HotFixTest {
 
 	public static void main(String[] args) {
-		//프로젝트명->오른쪽->remote->switch->new branch->hotfix
-		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+		//hotfix브랜치에서 저장점을 한개 더 가지기 위한 소스 추가
+		System.out.println("hotfix 브랜치에서 2번째로 추가한 소스임.");
 	}
 
 }
